6bf695e40fab92a9948b73b2b0e91a46bec26204
Sophia Yin <sjyin2013@gmail.com>
2020-02-25 19:28:39 -0800

    added in a table view for home view controller

--- QuestPrototype1/QuestPrototype1/VCs/LoginViewController.swift
+++ QuestPrototype1/QuestPrototype1/VCs/LoginViewController.swift
@@ -72,7 +72,7 @@ class LoginViewController: UIViewController {
            
        }
     func transitionToHome(){
-          let homeViewController = storyboard?.instantiateViewController(identifier:Constants.Storyboard.homeViewController) as? HomeViewController
+          let homeViewController = storyboard?.instantiateViewController(identifier:Constants.Storyboard.homeViewController) as? navigationController
            view.window?.rootViewController = homeViewController
            view.window?.makeKeyAndVisible()
            
